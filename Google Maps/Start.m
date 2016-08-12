2a1e52d9c427509dfa34b79be572f4fa6d0cfc1a
Daniel <Daniel@daniels-mac.local>
2016-08-12 17:35:23 -0600

    Maps added but only pointer show

--- Google Maps/Start.m
+++ Google Maps/Start.m
@@ -1,27 +1,47 @@
-//
 //  ViewController.m
 //  Google Maps
 //
-//  Created by Daniel on 8/12/16.
-//  Copyright © 2016 DanielCompany. All rights reserved.
+//  Created by Walter Gonzalez Domenzain on 01/07/16.
+//  Copyright © 2016 Smartplace. All rights reserved.
 //
 
 #import "Start.h"
+@import GoogleMaps;
 
 @interface Start ()
 
 @end
 
-@implementation Start
+@implementation Start {
+    GMSMapView *mapView_;
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    [self createMap];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (void)createMap {
+    // Create a GMSCameraPosition that tells the map to display the
+    // coordinate -33.86,151.20 at zoom level 6.
+    GMSCameraPosition *camera = [GMSCameraPosition cameraWithLatitude:-33.86
+                                                            longitude:151.20
+                                                                 zoom:5];
+    mapView_ = [GMSMapView mapWithFrame:CGRectZero camera:camera];
+    mapView_.myLocationEnabled = YES;
+    self.view = mapView_;
+    
+    // Creates a marker in the center of the map.
+    GMSMarker *marker = [[GMSMarker alloc] init];
+    marker.position = CLLocationCoordinate2DMake(-33.85, 151.20);
+    marker.title = @"UAG";
+    marker.snippet = @"Clase de Maestría";
+    marker.map = mapView_;
 }
 
 @end
